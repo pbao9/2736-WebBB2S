d05b11c4c9887b723edecbeb80e080f33433a68e
Pham Bao <baobaoo5533@gmail.com>
2024-08-23 13:05:02 +0700

    add

--- resources/views/public/layouts/scripts.blade.php
+++ resources/views/public/layouts/scripts.blade.php
@@ -1,7 +1,5 @@
 <script src="{{ asset('/public/libs/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('public/libs/tabler/dist/js/tabler.min.js') }}"></script>
-<script defer src="{{ asset('/public/libs/flickity/js/flickity.pkgd.min.js') }}"></script>
-<script defer src="{{ asset('/public/libs/owl-carousel/js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('/public/libs/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('/public/libs/datatables/plugins/bs5/js/dataTables.bootstrap5.min.js') }}"></script>
 <script src="{{ asset('/public/libs/datatables/plugins/buttons/js/dataTables.buttons.min.js') }}"></script>
@@ -10,6 +8,8 @@
 <script src="{{ asset('/public/libs/Parsley.js-2.9.2/parsley.min.js') }}"></script>
 <script src="{{ asset('/public/libs/jquery-throttle-debounce/jquery.ba-throttle-debounce.min.js') }}"></script>
 
+<script defer src="{{ asset('/public/libs/owl-carousel/js/owl.carousel.min.js') }}"></script>
+<script defer src="{{ asset('/public/libs/flickity/js/flickity.pkgd.min.js') }}"></script>
 
 @stack('libs-js')
 
